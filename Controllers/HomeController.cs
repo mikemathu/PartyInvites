5ada9e7e390c6e03f79e38c0536ac981bde45c12
Michael Mathu <mathumichael17@gmail.com>
2022-05-15 14:26:26 +0300

    adding dynamic output

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,11 @@ namespace PartyInvites.Controllers
     public class HomeController : Controller
     {
         //Action Method
-        public string Index()
+        public ViewResult Index()
         {
-            return "Hello Mike";
+            int hour = DateTime.Now.Hour;
+            ViewBag.Salamu = hour < 12 ? "Good Morming" : "Good Afternoon";
+            return View("MyView");
         }
     }
 }
