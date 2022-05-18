79e3f4231a466cdf113c9cb61d30593dbd74600a
Michael Mathu <mathumichael17@gmail.com>
2022-05-18 08:56:11 +0300

    using model binding

---
+++ Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        public static class Repository
+        {
+            private static List<GuestResponse> Responses
+            {
+                get
+                {
+                    return Responses;
+                }
+            }
+            public static void AddResponse(GuestResponse response)
+            {
+                responses.Add(response);
+            }
+        }
+    }
+}
