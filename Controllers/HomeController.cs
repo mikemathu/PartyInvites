a31c32c4fdbe73df3e433d7a19b4c849f1047452
Michael Mathu <mathumichael17@gmail.com>
2022-05-18 12:11:08 +0300

    designing data model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,10 +9,19 @@ namespace PartyInvites.Controllers
         //Action Method
         public ViewResult Index()
         {
+            /*This provide data for the view by assigning a value to the ViewBag.Greeting property. The Greeting
+            property didn’t exist until the moment I assigned the value—this allows me to pass data from the controller
+            to the view in a free and fluid manner, without having to define classes ahead of time.*/
             int hour = DateTime.Now.Hour;
             ViewBag.Salamu = hour < 12 ? "Good Morming" : "Good Afternoon";
+            ViewBag.Subject = "Work Harder";
+
             return View("MyView");
         }
+
+        /*The RsvpForm action method calls the View method without an argument, which tells MVC to render 
+          the default view associated with the action method, which is a view with the same name as the action 
+           method, in this case, RsvpForm.cshtml*/
 
         [HttpGet]
         public ViewResult RsvpForm()
